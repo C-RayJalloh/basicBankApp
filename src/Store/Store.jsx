8647d0cd8e5598c5bb42af5b9e35595dd0cb6bb5
“Jahrulo” <mserayjay@gmail.com>
2024-03-13 21:16:15 +0000

    changes

--- src/Store/Store.jsx
+++ src/Store/Store.jsx
@@ -1,1 +1,26 @@
 // Redux State Management
+
+
+// initial State
+const initalState = {
+    balance: 0,
+    loan: 0,
+    loanPurpose: ""
+}
+
+
+// Account Operations Reducer
+function reducer(state = initalState, action){
+     switch(action.type) {
+        case 'account/Deposite':
+            return {...state, balance: state.balance + action.payload};
+
+            case 'account/Deposite':
+                return {...state, balance: state.balance + action.payload};
+       
+
+            default: 
+             return state;
+     }
+
+}
