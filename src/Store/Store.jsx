59c7608306b586a3ed3f87fdd0be1040a3f4070c
“Jahrulo” <mserayjay@gmail.com>
2024-03-13 21:44:11 +0000

    changes

--- src/Store/Store.jsx
+++ src/Store/Store.jsx
@@ -1,5 +1,7 @@
 // Redux State Management
 
+import { createStore, combineReducers} from "redux";
+
 
 // initial State
 const initalState = {
@@ -10,17 +12,43 @@ const initalState = {
 
 
 // Account Operations Reducer
-function reducer(state = initalState, action){
-     switch(action.type) {
-        case 'account/Deposite':
-            return {...state, balance: state.balance + action.payload};
+function accountReducer(state = initalState, action){
+     switch (action.type) {
+       case "account/Deposite":
+         return { ...state, balance: state.balance + action.payload };
 
-            case 'account/Deposite':
-                return {...state, balance: state.balance + action.payload};
-       
+       case "account/Withdraw":
+         return { ...state, balance: state.balance - action.payload };
 
-            default: 
-             return state;
+       case "account/RequestLoan":
+         if (state.loan > 0) return state;
+         return {
+           ...state,
+           loan: +action.payload.amount,
+           loanPurpose: action.payload.purpose,
+           balance: state.balance + action.payload.amount,
+         };
+
+       case "account/PayLoan":
+         return {
+           ...state,
+           loan: 0,
+           loanPurpose: "",
+           balance: state.balance - state.loan,
+         };
+
+       default:
+         return state;
      }
 
 }
+
+// Redux Reducer Selector - with the combine Reducers hook
+const rootReducer = combineReducers({
+    account: accountReducer,
+    // customer: customerReducer,
+  });
+
+
+// create the store - statemanagement STORE
+const Store = createStore(rootReducer)
