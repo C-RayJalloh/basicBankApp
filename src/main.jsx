b9e7e4b63342567df37a117478e595a50e92a15e
“Jahrulo” <mserayjay@gmail.com>
2024-03-14 11:54:23 +0000

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import store from './Store/Store.jsx'
-import { Provider } from 'react-redux'
+
+// store.dispatch({ type: "account/Deposit", payload: 250})
+// console.log(store.getState());
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
-  </Provider>,
+
+  </React.StrictMode>
+
 )
