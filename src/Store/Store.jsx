3745f44f8dfefab0a5fdad22db1af7b8eceb8e12
“Jahrulo” <mserayjay@gmail.com>
2024-03-14 11:49:59 +0000

    changes

--- src/Store/Store.jsx
+++ src/Store/Store.jsx
@@ -3,8 +3,8 @@
 // Redux State Management
 
 import { createStore, combineReducers} from "redux";
-
-
+import accountReducer from "../Features/Accounts/slices/AccountSlice";
+import customerReducer from "../Features/Customers/slices/CustomerSlice";
 
 
 
@@ -17,35 +17,6 @@ const rootReducer = combineReducers({
 
 // create the store 
 const store = createStore(rootReducer);
-
-// // dispatching the reducer Actions
-// store.dispatch({ type: "account/Deposit", payload: 500 });
-// // logging the current STATE
-// console.log(store.getState());
-// store.dispatch({ type: "account/Withdraw", payload: 200 });
-// console.log(store.getState());
-
-
-
-
-  
-  // // HERE TO DISPATCH WE TAKE THE ACTION CREATOR AND USE IT AS AN ARGUMENT
-  // store.dispatch(deposit(500));
-  // console.log(store.getState());
-  // store.dispatch(withdraw(200));
-  // console.log(store.getState());
-  
-  // store.dispatch(requestLoan(1000, "Buy a cheap car"));
-  // console.log(store.getState());
-  // store.dispatch(payLoan());
-  // console.log(store.getState());
-
-  // // DISPATCHING WITH ACTION CREATORS
-  // store.dispatch(createCustomer("ray jahrulo", "08397823"))
-  // console.log(store.getState());
-
-
-// USING THE DISPATCH METHOD WITHOUT CREATORS
 
 
 export default store;
